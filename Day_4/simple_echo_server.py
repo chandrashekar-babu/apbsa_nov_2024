d5158f1ca3fd64f8d141a718d74f5db68837504b
Chandrashekar Babu <chandra@slashprog.com>
2024-11-21 12:52:11 +0530

    simple_echo_client implementation

--- Day_4/simple_echo_server.py
+++ Day_4/simple_echo_server.py
@@ -2,13 +2,17 @@ from socket import socket, AF_INET, SOCK_STREAM, SOMAXCONN
 
 def echo_handler(conn):
     ins, outs = conn.makefile("r"), conn.makefile("w")
-    for line in ins:
-        print(f"Client sent: {line}")
-        print(line.upper(), file=outs, flush=True)
-        if "exit" in line:
-            break
-    ins.close()
-    outs.close()
+    try:
+        for line in ins:
+            print(f"Client sent: {line}")
+            print(line.upper(), file=outs, flush=True)
+            if "exit" in line:
+                break
+    except ConnectionResetError:
+        print("Client closed the connection...")
+    finally:
+        ins.close()
+        outs.close()
 
 if __name__ == '__main__':
     HOST = "localhost"
